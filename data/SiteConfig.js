8e5ab63dea1a609e65642e4476a3cd09e170714a
riguelbf <riguel@dkosoftware.com.br>
2018-03-28 09:17:35 -0300

    change background cover image

--- data/SiteConfig.js
+++ data/SiteConfig.js
@@ -11,7 +11,7 @@ module.exports = {
   siteDescription:
     "Just a blog from a simple developer", // Website description used for RSS feeds/meta description tag.
   siteCover:
-    "https://haysclark.github.io/gatsby-starter-casper/images/blog-cover.jpg", // Optional, the cover image used in header for home page. e.g: "/images/blog-cover.jpg",
+    "http://lancedesign.org/en/wp-content/uploads/2016/05/Website-Design-Background-1.jpg", // Optional, the cover image used in header for home page. e.g: "/images/blog-cover.jpg",
   siteNavigation: true, // If navigation is enabled the Menu button will be visible
   siteRss: "/rss.xml", // Path to the RSS file.
   siteRssAuthor: "Riguel Figueiro", // The author name used in the RSS file
@@ -45,7 +45,7 @@ module.exports = {
   ],
   // Copyright string for the footer of the website and RSS feed.
   copyright: {
-    label: "Riguel Figueiro - 2018" // Label used before the year
+    label: "Riguel Figueiro" // Label used before the year
     // year: "2018" // optional, set specific copyright year or range of years, defaults to current year
     // url: "https://www.gatsbyjs.org/" // optional, set link address of copyright, defaults to site root
   },
